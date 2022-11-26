3474fef53573e5434ddf3eb52b27f8cfbdf79797
JoaoErick <jerick1415@outlook.com>
2022-11-25 23:02:04 -0300

    Feat: add age group charts

--- main.R
+++ main.R
@@ -11,14 +11,18 @@
 #        João Pedro Rios e Gabriel Carvalho.                   #
 ################################################################
 
+# --- Configurações ---
+
 rm(list=ls())
 
 # Instalação de pacotes
-# install.packages('readxl')
-# install.packages('descr')
-# 
-# library(readxl)
-# library(descr)
+install.packages('readxl')
+install.packages('descr')
+install.packages('RColorBrewer')
+
+library(readxl)
+library(descr)
+library(RColorBrewer)
 
 # Importando banco em excel
 dataset = read_excel("dataset.xlsx")
@@ -29,42 +33,73 @@ View(dataset)
 # Codificando a base de dados em variáveis quantitativas discretas
 source("./utils/codeNumber.R")
 
-summary(dataset$SEXO)
-
 attach(dataset)
 
-# ---------------------
+# --- Geração de gráficos ---
 
-#make slices
+x11()
+colors = brewer.pal(3, "Set2")
+
+# Obtendo a frequência de cada sexo
 slices = table (SEXO)
 
-# Calculate the percentage for each day, rounded to one decimal place
+# Calculando a frequência relativa de cada sexo
 slices_labels = round(slices/sum(slices) * 100, 1)
-
-# Concatenate a '%' char after each value
 slices_labels = paste(slices_labels, "%", sep="")
-
-colors = c("yellow2", "blue2")
 
 pie(table(SEXO), main = "Respondentes por sexo", labels=slices_labels, cex=0.7, col = colors)
 legend(.9, .9, c("Masculino","Feminino"), fill = colors)
 
 # -------------------
 
-#make slices
+# Obtendo a frequência de cada relação sexo x consome bebida
 slices = table (SEXO, TOMA_BEBIDA)
 
-# Calculate the percentage for each day, rounded to one decimal place
+# Calculando a frequência relativa de cada relação sexo x consome bebida
 slices_labels = round(slices/sum(slices) * 100, 1)
-
-# Concatenate a '%' char after each value
 slices_labels = paste(slices_labels, "%", sep="")
-
-colors = c("green2","yellow2","red2", "blue2")
 
 pie(table(SEXO, TOMA_BEBIDA), main = "Relação Sexo x Bebida", labels=slices_labels, cex=0.7, col = colors)
 legend(.9, .9, c("Masc. bebe","Fem. bebe","Masc. não bebe", "Fem. não bebe"), fill = colors)
 
 # text(locator(n=1), paste(round(prop.table(table(dados$educa))[1], digits=2)*100,"%"))
 
-boxplot(SEXO,TOMA_BEBIDA)
+# -------------------
+
+# Exibindo gráfico em barras das faixas etárias
+
+ages = append(rep(table(FAIXA_ETARIA)), c(0, 0))
+barplot(
+  ages, 
+  ylab = "Nº de pessoas",
+  xlab = "Faixas etárias",
+  ylim = c(0,50), 
+  names = c("16-20 anos", "21-25 anos", "26-30 anos", "30+ anos"),
+  col = colors
+)
+
+# -------------------
+
+# Exibindo gráfico em barras das faixas etárias que consomem bebida
+barplot(
+  table (FAIXA_ETARIA,TOMA_BEBIDA)[,1], 
+  main = "Número de pessoas que consomem bebida alcoólica",
+  ylab = "Nº de pessoas",
+  xlab = "Faixas etárias",
+  ylim = c(0,35), 
+  names = c("16-20 anos", "21-25 anos"),
+  col = colors
+)
+
+# -------------------
+
+# Exibindo gráfico em barras das faixas etárias que não consomem bebida
+barplot(
+  table (FAIXA_ETARIA,TOMA_BEBIDA)[,2], 
+  main = "Número de pessoas que não consomem bebida alcoólica",
+  ylab = "Nº de pessoas",
+  xlab = "Faixas etárias",
+  ylim = c(0,20), 
+  names = c("16-20 anos", "21-25 anos"),
+  col = colors
+)
